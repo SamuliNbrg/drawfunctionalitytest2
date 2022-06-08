1b6248d246da4fd3c5c3687f0142d007cdfb537a
Samuli Nordberg <43672827+SamuliNbrg@users.noreply.github.com>
2022-06-08 08:04:54 +0300

    Update draw.js
    Addad backgroud grid and redraw upon resize

--- draw.js
+++ draw.js
@@ -10,13 +10,68 @@ window.onload = function() {
 
   var lastx;
   var lasty;
+  
+  initialize();
 
-  context.strokeStyle = "#000000";
-  context.lineCap = 'round';
-  context.lineJoin = 'butt';
-  context.lineWidth = 7;
+  function initialize() {
+    // Register an event listener to call the resizeCanvas() function 
+    // each time the window is resized.
+    window.addEventListener('resize', resizeCanvas, false);
+    // Draw canvas border for the first time.
+    resizeCanvas();
+  }
+
+  // draws a grid
+  function redraw() {
+	context.strokeStyle = "#d0ced3";
+	context.lineWidth = 6;
+	
+	//rectangle around the canvas
+	context.strokeRect(2, 2, window.innerWidth-4, window.innerHeight-4);
+	
+	//first diagonal
+    context.beginPath(); 
+	context.moveTo(0, 0);
+	context.lineTo(window.innerWidth, window.innerHeight);
+	context.stroke();
+	context.closePath();
+	
+	//second diagonal
+	context.beginPath(); 
+	context.moveTo(0, window.innerHeight);
+	context.lineTo(window.innerWidth, 0);
+	context.stroke();
+	context.closePath();
+	
+	//vertical center
+	context.beginPath(); 
+	context.moveTo(window.innerWidth/2, 0);
+	context.lineTo(window.innerWidth/2, window.innerHeight);
+	context.stroke();
+	context.closePath();
+	
+	//horizontal center
+	context.beginPath(); 
+	context.moveTo(0, window.innerHeight/2);
+	context.lineTo(window.innerWidth, window.innerHeight/2);
+	context.stroke();
+	context.closePath();
+  }
+
+  // Runs each time the DOM window resize event fires.
+  // Resets the canvas dimensions to match window,
+  // then draws the new borders accordingly.
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    redraw();
+  }
 
   function line(fromx,fromy, tox,toy) {
+	context.strokeStyle = "#000000";
+	context.lineCap = 'round';
+	context.lineJoin = 'butt';
+	context.lineWidth = 8;
     context.beginPath();
     context.moveTo(fromx, fromy);
     context.lineTo(tox, toy);
@@ -42,10 +97,4 @@ window.onload = function() {
     lastx = newx;
     lasty = newy;
   }
-
-
-  var clearButton = document.getElementById('clear')
-  clearButton.onclick = clear
-
-  clear()
 }
